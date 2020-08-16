b460688b2f4da580c42fe308e6a8b9cff698e90f
Leonardo Lourenco <lnddru@gmail.com>
2020-08-16 17:34:16 -0400

    Add files via upload

---
+++ Aula-03/Aula-03.c
@@ -0,0 +1,24 @@
+#include<stdio.h>  // Biblioteca para entrada e saída de dados
+
+
+
+int main(){
+	
+	//significa exibir dados ou imprimir 
+	printf("Hello Word uhuuuuuu");
+	
+	return 0;
+	
+	}
+
+/*
+/int main(){
+	
+	//significa exibir dados ou imprimir 
+	printf("Hello Word uhuuuuuu");
+	
+	return 0;
+	
+	}
+
+*/
