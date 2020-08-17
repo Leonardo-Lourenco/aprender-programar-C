47ccffc7ef0f0506505fb342abeb004dd308f7fa
Leonardo Lourenco <lnddru@gmail.com>
2020-08-17 19:22:57 -0400

    Add files via upload

---
+++ Aula-04/Aula-04.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+// Entrada e Saídas de dados
+
+int main(){
+	
+	// SAÍDAS DE DADOS
+	
+	//int soma = 20;	
+	//printf("O valor é:  %i", soma);
+	
+	
+	
+	// SAÍDA DE VÁRIOS DADOS
+	//int valor1 = 10;
+	
+	//float valor2 = 20.11;
+	
+	//char a = 'l';
+	
+	//printf("%i\n%f\n%c", valor1, valor2, a); //\n
+	
+	
+	// PEDINDO VALORES PARA OS USUÁRIOS E EXININDO ESTE VALORES
+	
+	int valor1, valor2;
+	
+	printf("Informe um valor:  ");
+	scanf("%i", &valor1);// & salvar o endereço de memória de um determinado
+	
+	
+	printf("Informe o segundo valor:  ");
+	scanf("%i", &valor2);
+	
+	// Exibindo valores na tela
+	
+	printf("O primeiro valor informado é:  %i \n ", valor1);
+	printf("O segundo valolr informado é:  %i", valor2);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	}
