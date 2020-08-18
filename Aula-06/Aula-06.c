693509c2e702530fd0745a9c85f9a2424f18f503
Leonardo Lourenco <lnddru@gmail.com>
2020-08-18 18:23:09 -0400

    Add files via upload

---
+++ Aula-06/Aula-06.c
@@ -0,0 +1,38 @@
+#include<stdio.h> 
+
+// Operadores de ATRIBUIÇÃO
+
+int main(){  
+	
+	/*int num;
+	
+	//num = 20;
+	
+	//printf("O valor informado é:  %i", num);
+	
+	int num, num2, num3;
+	
+	num = num2 = num3 = 20;
+	
+	printf("Os números: %i %i %i \n ", num, num2, num3);
+	
+	*/
+	
+	
+	int num;
+	
+	num = 30;
+	
+	//num = num + 50;
+	num /=2;
+	
+	
+	printf("O valor é: %i", num);
+	
+	
+	
+	
+	
+	return 0; 
+	
+	}
