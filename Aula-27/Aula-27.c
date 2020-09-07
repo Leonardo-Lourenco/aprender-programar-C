977ba12416f4a602354aa0a2dbd550d700a57d83
Leonardo Lourenco <lnddru@gmail.com>
2020-09-07 12:04:50 -0400

    Update Aula-27.c

--- Aula-27/Aula-27.c
+++ Aula-27/Aula-27.c
@@ -1,85 +1,42 @@
 #include<stdio.h> 
 
 /* 
+    Sequência de Fibonacci
     
-    Neste tutorial, você aprenderá a trabalhar com arrays. Você aprenderá a declarar, 
-    inicializar e acessar elementos de um array com a ajuda de exemplos.
-     
-    Um VETOR é uma variável que pode armazenar vários valores. Por exemplo, 
-    se você deseja armazenar 100 inteiros, pode criar uma matriz para ele.
-    
-    int num[100]; 
-    
-    É importante observar que o tamanho e o tipo de uma VETOR não podem ser alterados depois de declarada.
+    Neste exemplo, você aprenderá a exibir a sequência de Fibonacci dos primeiros n números (inseridos pelo usuário).
     
     
-    1 -  É possível inicializar um array durante a declaração. Por exemplo,
-    
-    int num[5] = {19, 10, 8, 17, 9};
-  
-    
-    
-    
-    
-    
-    2 - Você também pode inicializar um array como este.
-    
-    int mnum[] = {19, 10, 8, 17, 9};
-    
-    
-    Aqui, não especificamos o tamanho. No entanto, o compilador sabe que seu tamanho é 5, pois o estamos inicializando com 5 elementos.
-    
-    
-    SAÍDA DO PROGRAMA:
-    
-    num [0] é igual a 19
-	num [1] é igual a 10
-	num [2] é igual a 8
-	num [3] é igual a 17
-	mnum [4] é igual a 9
-    
-    
-    Alterar o valor dos elementos da matriz
-    
-    int num[5] = {19, 10, 8, 17, 9}
+	A sequência de Fibonacci é uma sequência em que o próximo termo é a soma dos dois termos anteriores. 
+	Os primeiros dois termos da sequência de Fibonacci são 0 seguidos por 1.
+	
+	A sequência de Fibonacci: 0, 1, 1, 2, 3
 
-	// Alterar valor na posição 2 para  -1
-	mnum[2] = -1;
-
-	// Alterar valor na posição 4 para 0
-	num[4] = 0;
-    
-    
+ 
   */
   
   
-  // Programa pede para informar 5 VALORES depois armazena e exibe estes valores
-  
-  
-  int main() {
-	  
-	  int num[5];
-	  
-	  
-	  printf("Informe 5 valores: ");
-	  
-	  for(int i =0; i < 5; i++){
-		  
-		  scanf("%i", &num[i]);
-		  
-		  }
-	  
-	  printf("Os valores informados são  ");
-	  
-	  for(int i = 0; i<5;i++){
-		  
-		  printf("%i\n", num[i]);
-		  
-		  }
-	  
-	  
-	 
-	return 0;
-  
-  
+int main() {
+
+	
+	int i, n, t1 = 0, t2 = 1, proxTermo;
+	
+	
+	printf("Informe a quantidade de termos:  ");
+	scanf("%i", &n);
+	
+	
+	printf("\n SERIE DE FIBONACCI \n");
+	
+	for(i = 1; i <= n; i++){
+		
+		printf("%i, ", t1);
+		proxTermo = t1 + t2; // somando o primeiro termo mais o segundo termo t1 + t2 = 1
+		t1 = t2; // 1
+		t2 = proxTermo; // 0 1 1   = t1 = 1 t2 = 1  // 0 1 1  
+			
+		
+		}
+	
+   return 0;
 }
+
